7ac90b24d3fea89754f5ddf1b94fb5ac71d18056
Tianfu Ma <matianfu@gmail.com>
2016-04-05 20:06:02 +0800

    Create minimal.sh

---
+++ minimal.sh
@@ -0,0 +1,36 @@
+# 
+mkdir rootfs
+mount /dev/sda1 rootfs
+cd rootfs
+
+#
+tar xf ../ubuntu-core-16.04-beta2-core-amd64.tar.gz
+
+# 
+echo "wisnuc"                      > etc/hostname
+echo "127.0.0.1 localhost"         > etc/hosts
+echo "127.0.1.1 wisnuc"            > etc/hosts
+echo "[Match]"                     > /etc/systemd/network/wired.network
+echo "Name=enp1s0"                >> /etc/systemd/network/wired.network
+echo "[Network]"                  >> /etc/systemd/network/wired.network
+echo "DHCP=ipv4"                  >> /etc/systemd/network/wired.network
+echo "nameserver 208.67.222.222"   > etc/resolv.conf
+
+mount -t devtmpfs none dev
+mount -t proc none proc
+mount -t sysfs none sys
+chroot .
+
+apt-get update
+apt-get -y install linux-image-4.4.0-16-generic net-tools iproute2 nano
+
+echo "root:123456" | chpasswd
+
+echo "/dev/sda1 / ext4 defaults 1 1" > etc/fstab
+
+systemctl enable systemd-networkd
+systemctl enable systemd-resolved
+
+exit
+umount sys proc dev
+
